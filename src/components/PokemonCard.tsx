06017e540f6b16357538427d950f589c020f5468
BrunoBernard979 <bruno.bernard33@gmail.com>
2024-12-12 15:26:44 +0100

    résultat de la quete 03

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,11 +1,26 @@
+const PokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+// 
 const PokemonCard: React.FC=() =>{
+  const Pokemon = PokemonList[1];
+// 
   return (
-    <figure>
-    <img 
-      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
-      alt="Bulbasaur"
-      />
-    <figcaption>Bulbasaur</figcaption>
+  <figure>
+    {Pokemon.imgSrc ? (
+        <img src={Pokemon.imgSrc} alt={Pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+
+      <figcaption>{Pokemon.name}</figcaption>
   </figure>
   );
 };
