979f8af52673d1b4c50c17a8837b36ec175019e9
BrunoBernard979 <bruno.bernard33@gmail.com>
2024-12-12 11:54:10 +0100

    final résult

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -3,8 +3,9 @@ const PokemonCard: React.FC=() =>{
     <figure>
     <img 
       src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
-      alt="Bulbasaur" 
-    />
+      alt="Bulbasaur"
+      />
+    <figcaption>Bulbasaur</figcaption>
   </figure>
   );
 };
