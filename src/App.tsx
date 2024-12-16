61d21901740df1b22af7101bd33ebe8c16afe2cd
BrunoBernard979 <bruno.bernard33@gmail.com>
2024-12-16 16:56:55 +0100

    Exercice04

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import "./App.css"
 import PokemonCard from "./components/PokemonCard";
+const PokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 const App: React.FC = () => {
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={PokemonList[0]} />
     </div>
   );
 };
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,26 +1,14 @@
-const PokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+
 // 
-const PokemonCard: React.FC=() =>{
-  const Pokemon = PokemonList[1];
-// 
+function  PokemonCard ({pokemon})  {
   return (
   <figure>
-    {Pokemon.imgSrc ? (
-        <img src={Pokemon.imgSrc} alt={Pokemon.name} />
+    {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
       ) : (
         <p>???</p>
       )}
-
-      <figcaption>{Pokemon.name}</figcaption>
+      <figcaption>{pokemon.name}</figcaption>
   </figure>
   );
 };
